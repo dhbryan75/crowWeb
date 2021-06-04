b6372fa57473cc83ebbcf92765b6fddfdffd3496
KDH\DoHyun <dhbryan75@gmail.com>
2021-06-04 17:04:54 +0900

    초기 파일구조 세팅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Header from './Pages/Common/Header';
+import HomePage from './Pages/Home/';
+import Footer from './Pages/Common/Footer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    width: window.innerWidth,
+    height: window.innerHeight
+  }
+
+  onResize = e => {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.onResize);
+  }
+
+  render() {
+    const { width, height } = this.state;
+    return (
+      <div className="app">
+        <Header />
+        <Router>
+          <Switch>
+            <Route path="/">
+              <HomePage width={width} height={height}/>
+            </Route>
+          </Switch>
+        </Router>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./style.css";
+
+class HomePage extends React.Component {
+    render() {
+        const { width, height } = this.props;
+        return (
+            <div className="home">
+                
+            </div>
+        );
+    }
+}
+
+export default HomePage;
